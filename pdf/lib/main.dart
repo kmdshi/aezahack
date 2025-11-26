e4dd5d911f0c041c35887afaa52455f10dead72c
kmdshi <lipatovb@gmail.com>
2025-11-26 11:20:36 +0300

    trans system area color

--- pdf/lib/main.dart
+++ pdf/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pdf_app/core/widgets/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+
   runApp(const App());
 }
